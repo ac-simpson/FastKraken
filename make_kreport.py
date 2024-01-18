bf44e4627c2b51c98233007129f7dbb7436ec45a
A.C. Simpson <acsimpson@gmail.com>
2024-01-17 16:27:57 -0800

    Adding make_kreport mod
    Modification of Jennifer Lu's make_kreport.py from KrakenTools to prevent taxonomy db from being loaded multiple times - when using a very large database and many many files, this makes the process very slow

--- make_kreport.py
+++ make_kreport.py
@@ -22,18 +22,25 @@
 #Jennifer Lu, jlu26@jhmi.edu
 #Updated: 04/15/2020
 #
+# Edited by Anna Simpson, asimpson@jpl.nasa.gov, 10-22-2023 for faster output of multiple files
+# To accommodate her 578 bins and enormous database
+# :(
+#
 #This program creates the kraken report file from
 #the make_ktaxonomy.py output and the kraken output file
 #
 #Required Parameters:
-#   -i,-k,--kraken X....................kraken output file
-#   -t,--taxonomy X.....................taxonomy file 
-#   -o, --output X......................output kraken report file 
+#   -i,-k,--kraken X......................kraken output file
+#   OR
+#   -idir, -kdir, --kraken_dir X..............directory of ONLY kraken output files
+#   -t,--taxonomy X.....................taxonomy file
+#   -o, --output X......................output kraken report file
 #Optional Parameters:
 #   -h, --help..........................show help message.
 #################################################################################
 import os, sys, argparse
 import operator
+import glob
 from time import gmtime
 from time import strftime 
 #################################################################################
@@ -63,16 +70,51 @@ class Tree(object):
 def main():
     #Parse arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i','--input', '-k','--kraken', dest='kraken_file', required=True,
+    infile_type = parser.add_mutually_exclusive_group(required=True)
+    outfile_type = parser.add_mutually_exclusive_group(required=True)
+    infile_type.add_argument('-i','--input','-k','--kraken', dest='kraken_file',
         help='Kraken output file (5 tab-delimited columns, taxid in 3rd column)')
+    infile_type.add_argument('-idir','--input_dir','-kdir', '--kraken_dir', dest='kraken_dir',
+        help='Directory containing only Kraken output files (5 tab-delimited columns, taxid in 3rd column)')
     parser.add_argument('-t','--taxonomy', dest='tax_file', required=True,
         help='Output taxonomy file from make_ktaxonomy.py')
-    parser.add_argument('-o','--output',dest='out_file', required=True,
-        help='Output kraken report file')
+    outfile_type.add_argument('-o','--output',dest='out_file',
+        help='Output kraken report file name (only use with single input file')
+    outfile_type.add_argument('-odir', '--output_dir', dest='out_file_dir',
+        help='Directory to store output kraken reports (will replace input file extension with _kreport.txt)')
     parser.add_argument('--use-read-len',dest='use_read_len',
         action='store_true',default=False, required=False,
         help='Make report file using sum of read lengths [default: read counts]')
     args = parser.parse_args()
+
+    ##
+
+    ## Define job type and look for inconsistencies in arguments
+    if args.kraken_file and args.out_file:
+        job_type='single_file'
+    elif args.kraken_dir and args.out_file_dir:
+        job_type='multiple_files'
+        if os.path.isdir(args.out_file_dir):
+            pass
+        else:
+            os.mkdir(args.out_file_dir)
+    else:
+        print('Input and output arguments do not match (must provide input file and output file name, or input directory and output directory name')
+        sys.exit(0)
+
+    ## Create list of input files - either single kraken output file
+    ## or list of files in provided directory of kraken output files
+    kraken_file_list=[]
+    if args.kraken_file:
+        kraken_file_list.append(args.kraken_file)
+    else:
+        for file in glob.glob(args.kraken_dir+'/*'):
+            if file not in kraken_file_list:
+                kraken_file_list.append(file)
+            else:
+                print(file, "is duplicated in directory")
+                ## What to do in this case?
+
 
     #Start Program
     time = strftime("%m-%d-%Y %H:%M:%S", gmtime())
@@ -103,92 +145,118 @@ def main():
     sys.stdout.write("\r\t%i nodes saved\n" % (count_nodes))
     sys.stdout.flush()
     #STEP 2/4: READ KRAKEN FILE FOR COUNTS PER TAXID
-    read_count = 0
-    sys.stdout.write(">> STEP 2/4: Reading kraken file %s...\n" % args.kraken_file)
-    sys.stdout.write("\t%i million reads processed" % read_count)
-    sys.stdout.flush()
+    krakenfile_to_taxidinfo={}
+    track_read_counts={}
     #Save counts per taxid
-    taxid2counts = {}
-    taxid2allcounts = {} 
-    k_file = open(args.kraken_file,'r')
-    for line in k_file:
-        read_count += 1
-        if read_count % 1000 == 0:
-            sys.stdout.write('\r\t%0.3f million reads processed' % float(read_count/1000000.))
-            sys.stdout.flush()
-        l_vals = line.strip().split('\t')
-        taxid = l_vals[2]
-        count = 1
-        #If using read length instead of read counts
-        if args.use_read_len:
-            if '|' in l_vals[3]:
-                [len1,len2] = l_vals[3].split('|')
-                count = int(len1)+int(len2)
+    for krakenfile in kraken_file_list:
+        read_count = 0
+        sys.stdout.write(">> STEP 2/4: Reading kraken file %s...\n" % krakenfile)
+        sys.stdout.write("\t%i million reads processed" % read_count)
+        sys.stdout.flush()
+        taxid2counts = {}
+        taxid2allcounts = {}
+        k_file = open(krakenfile,'r')
+        for line in k_file:
+            read_count += 1
+            if read_count % 1000 == 0:
+                sys.stdout.write('\r\t%0.3f million reads processed' % float(read_count/1000000.))
+                sys.stdout.flush()
+            l_vals = line.strip().split('\t')
+            taxid = l_vals[2]
+            count = 1
+            #If using read length instead of read counts
+            if args.use_read_len:
+                if '|' in l_vals[3]:
+                    [len1,len2] = l_vals[3].split('|')
+                    count = int(len1)+int(len2)
+                else:
+                    count = int(l_vals[3])
+            #add to dictionaries
+            if taxid not in taxid2counts:
+                taxid2counts[taxid] = count
+                taxid2allcounts[taxid] = count
             else:
-                count = int(l_vals[3])
-        #add to dictionaries 
-        if taxid not in taxid2counts:
-            taxid2counts[taxid] = count
-            taxid2allcounts[taxid] = count
-        else:
-            taxid2counts[taxid] += count
-            taxid2allcounts[taxid] += count
-    k_file.close()
-    sys.stdout.write('\r\t%0.3f million reads processed\n' % float(read_count/1000000.))
-    sys.stdout.flush()
+                taxid2counts[taxid] += count
+                taxid2allcounts[taxid] += count
+        k_file.close()
+        sys.stdout.write('\r\t%0.3f million reads processed\n' % float(read_count/1000000.))
+        sys.stdout.flush()
+        krakenfile_to_taxidinfo[krakenfile]={"taxid2counts":taxid2counts,"taxid2allcounts":taxid2allcounts}
+        track_read_counts[krakenfile]=read_count
     #STEP 3/4: FOR EVERY TAXID PARSED, ADD UP TOTAL READS
-    sys.stdout.write(">> STEP 3/4: Creating final tree...\n")
-    for curr_tid in taxid2counts:
-        #Skip unclassified
-        if curr_tid == '0':
-            continue 
-        p_node = taxid2node[curr_tid].parent 
-        add_counts = taxid2counts[curr_tid] 
-        #Assign reads for node
-        taxid2node[curr_tid].lvl_reads += add_counts 
-        taxid2node[curr_tid].all_reads += add_counts 
-        while (p_node != None):
-            #Add child reads to parent node 
-            p_taxid = p_node.taxid
-            if p_taxid not in taxid2allcounts:
-                taxid2allcounts[p_taxid] = add_counts
-            else:
-                taxid2allcounts[p_taxid] += add_counts
-            p_node.all_reads += add_counts
-            #Get next parent node
-            p_node = p_node.parent
-    #STEP 4/4: PRINT REPORT FILE 
-    sys.stdout.write(">> STEP 4/4: Printing report file to %s...\n" % args.out_file)
-    o_file = open(args.out_file,'w')
-    #Write line for unclassified reads:
-    if '0'  in taxid2counts:
-        o_file.write("%6.2f\t" % (float(taxid2counts['0'])/float(read_count)*100))
-        o_file.write("%i\t%i\t" % (taxid2counts['0'],taxid2counts['0']))
-        o_file.write('U\t0\tunclassified\n')
-    #Get remaining lines 
-    parse_nodes = [root_node]
-    while len(parse_nodes) > 0:
-        curr_node = parse_nodes.pop(0)
-        curr_tid = curr_node.taxid 
-        #Print information for this level
-        o_file.write("%6.2f\t" % (float(taxid2allcounts[curr_tid])/float(read_count)*100))
-        o_file.write("%i\t" % taxid2allcounts[curr_tid])
-        if curr_tid not in taxid2counts:
-            o_file.write("0\t")
+    sys.stdout.write(">> STEP 3/4: Creating final tree(s)...\n")
+    for krakenfile in krakenfile_to_taxidinfo:
+        taxid2counts = krakenfile_to_taxidinfo[krakenfile]["taxid2counts"]
+        taxid2allcounts = krakenfile_to_taxidinfo[krakenfile]["taxid2allcounts"]
+        for curr_tid in taxid2counts:
+            #Skip unclassified
+            if curr_tid == '0':
+                continue
+            p_node = taxid2node[curr_tid].parent
+            add_counts = taxid2counts[curr_tid]
+            #Assign reads for node
+            taxid2node[curr_tid].lvl_reads += add_counts
+            taxid2node[curr_tid].all_reads += add_counts
+            while (p_node != None):
+                #Add child reads to parent node
+                p_taxid = p_node.taxid
+                if p_taxid not in taxid2allcounts:
+                    taxid2allcounts[p_taxid] = add_counts
+                else:
+                    taxid2allcounts[p_taxid] += add_counts
+                p_node.all_reads += add_counts
+                #Get next parent node
+                p_node = p_node.parent
+        krakenfile_to_taxidinfo[krakenfile] = {"taxid2counts": taxid2counts, "taxid2allcounts": taxid2allcounts}
+    #STEP 4/4: PRINT REPORT FILE
+    outfile_list=[]
+    for krakenfile in krakenfile_to_taxidinfo:
+        read_count=track_read_counts[krakenfile]
+        taxid2counts = krakenfile_to_taxidinfo[krakenfile]["taxid2counts"]
+        taxid2allcounts = krakenfile_to_taxidinfo[krakenfile]["taxid2allcounts"]
+        if job_type=="single_file":
+            outfile_name=args.out_file
         else:
-            o_file.write("%i\t" % taxid2counts[curr_tid])
-        o_file.write("%s\t" % curr_node.level_rank)
-        o_file.write("%s\t" % curr_tid)
-        o_file.write(" "*curr_node.level_num*2 + curr_node.name + "\n")
-        #Add children to list
-        for child in sorted(curr_node.children, key=operator.attrgetter('all_reads')):
-            if child.taxid not in taxid2allcounts:
-                continue
-            if taxid2allcounts[child.taxid] == 0:
-                continue
-            #Add to list 
-            parse_nodes.insert(0,child)    
-    o_file.close() 
+            if "." in krakenfile:
+                basename=".".join(krakenfile.split(".")[0:-1])
+            else:
+                basename=krakenfile
+            outfile_name=args.out_file_dir+"/"+basename.split("/")[-1]+"_kreport.txt"
+            ## Just in case some weirdo put all their unique sample info in the extension
+            if outfile_name in outfile_list:
+                outfile_name=args.out_file_dir+"/"+krakenfile.split("/")[-1]+"_kreport.txt"
+                print(outfile_name, "is not great - consider renaming your input files!")
+        sys.stdout.write(">> STEP 4/4: Printing report file to %s...\n" % outfile_name)
+        o_file = open(outfile_name,'w')
+        #Write line for unclassified reads:
+        if '0'  in taxid2counts:
+            o_file.write("%6.2f\t" % (float(taxid2counts['0'])/float(read_count)*100))
+            o_file.write("%i\t%i\t" % (taxid2counts['0'],taxid2counts['0']))
+            o_file.write('U\t0\tunclassified\n')
+        #Get remaining lines
+        parse_nodes = [root_node]
+        while len(parse_nodes) > 0:
+            curr_node = parse_nodes.pop(0)
+            curr_tid = curr_node.taxid
+            #Print information for this level
+            o_file.write("%6.2f\t" % (float(taxid2allcounts[curr_tid])/float(read_count)*100))
+            o_file.write("%i\t" % taxid2allcounts[curr_tid])
+            if curr_tid not in taxid2counts:
+                o_file.write("0\t")
+            else:
+                o_file.write("%i\t" % taxid2counts[curr_tid])
+            o_file.write("%s\t" % curr_node.level_rank)
+            o_file.write("%s\t" % curr_tid)
+            o_file.write(" "*curr_node.level_num*2 + curr_node.name + "\n")
+            #Add children to list
+            for child in sorted(curr_node.children, key=operator.attrgetter('all_reads')):
+                if child.taxid not in taxid2allcounts:
+                    continue
+                if taxid2allcounts[child.taxid] == 0:
+                    continue
+                #Add to list
+                parse_nodes.insert(0,child)
+        o_file.close()
     #End of program
     time = strftime("%m-%d-%Y %H:%M:%S", gmtime())
     sys.stdout.write("PROGRAM END TIME: " + time + '\n')
@@ -200,4 +268,3 @@ if __name__ == "__main__":
 #################################################################################
 ##################################END OF PROGRAM#################################
 #################################################################################
-
